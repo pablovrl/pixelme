82f17ce4c9157de53981a3179232304a009aab60
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-02-23 18:17:19 -0300

    update store

--- src/store/baseImageStore.ts
+++ src/store/baseImageStore.ts
@@ -1,6 +1,11 @@
 import { create } from "zustand";
 
 interface BaseImageStore {
+  size: {
+    width: number;
+    height: number;
+  }
+  updateSize: (size: { width: number; height: number }) => void;
   baseImage: string;
   updateBaseImage: (baseImage: string) => void;
   baseImageId: string;
@@ -10,6 +15,11 @@ interface BaseImageStore {
 }
 
 export const useBaseImageStore = create<BaseImageStore>((set) => ({
+  size: {
+    width: 0,
+    height: 0,
+  },
+  updateSize: (size: { width: number; height: number }) => set({ size }),
   baseImage: "",
   updateBaseImage: (baseImage: string) => set({ baseImage }),
   baseImageId: "",
