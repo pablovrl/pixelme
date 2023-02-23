705677e0311e84f1eb521ddbe572faf8857e7fd4
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-02-23 18:16:35 -0300

    create new components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+export function Footer() {
+  return (
+    <footer className="w-full flex flex-col items-center justify-center gap-4 text-white bg-gray-600 h-48 text-md">
+      <p className="font-bold">Made with ❤️ by Pablo Villarroel</p>
+      <div className="flex gap-4">
+        <a href="https://github.com/pablovrl" target="_blank">
+          Github
+        </a>
+        <a
+          href="https://linkedin.com/in/pablovillarroelantillanca"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+        <a href="https://pablovillarroel.me" target="_b lank">
+          Portfolio
+        </a>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/Landing.tsx
@@ -0,0 +1,26 @@
+import pixelatedMidu from "../assets/pixelated-midu.jpeg";
+import { Dropzone } from "./Dropzone";
+
+export function Landing() {
+  return (
+    <div className="flex" style={{ minHeight: "calc(100vh - 80px)" }}>
+      <div className="flex gap-16 items-center justify-center">
+        <div className="flex flex-col gap-10 w-full">
+          <img
+            className="rounded-3xl"
+            src={pixelatedMidu}
+            alt="pixelated midu"
+            width={300}
+          />
+          <div>
+            <h2 className="text-6xl mb-4">Pixelate your images</h2>
+            <p className="text-gray-400">
+              Select the areas you want to pixelate and that's it!
+            </p>
+          </div>
+        </div>
+        <Dropzone />
+      </div>
+    </div>
+  );
+}
