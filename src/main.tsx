ba99dc128a878c4ce3834e16cd54c50d5eadb5fa
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-02-23 18:16:42 -0300

    add Toaster

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import "@fontsource/poppins"
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Toaster />
     <App />
   </React.StrictMode>,
 )
